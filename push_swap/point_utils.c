6f8ee347aa5a188e633d32de5bb5311cf00dcd9b
MrM0lten <jisserst@c4b12c6.42berlin.de>
2022-12-25 20:04:38 +0100

    progress

--- push_swap/point_utils.c
+++ push_swap/point_utils.c
@@ -6,16 +6,17 @@
 /*   By: jisserst <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 19:26:17 by jisserst          #+#    #+#             */
-/*   Updated: 2022/12/25 19:26:18 by jisserst         ###   ########.fr       */
+/*   Updated: 2022/12/25 20:03:47 by jisserst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	get_elem(int *arr, int arr_len);
+
 t_2dpoint	*get_best_elem(t_2dpoint **arr)
 {
 	int	i;
-	int	temp_val;
 	int	arr_len;
 	int	arr_pos;
 	int	*totals;
@@ -34,21 +35,31 @@ t_2dpoint	*get_best_elem(t_2dpoint **arr)
 		else
 			totals[i] = ft_abs(arr[i]->val_a) + ft_abs(arr[i]->val_b);
 		i++;
-	}	
+	}
+	arr_pos = get_elem(totals, arr_len);
+	free(totals);
+	return (arr[arr_pos]);
+}
+
+static int	get_elem(int *arr, int arr_len)
+{
+	int	temp_val;
+	int	arr_pos;
+	int	i;
+
 	i = 0;
 	arr_pos = 0;
-	temp_val = totals[i];
+	temp_val = arr[i];
 	while (i < arr_len)
 	{
-		if (temp_val > totals[i])
+		if (temp_val > arr[i])
 		{
-			temp_val = totals[i];
+			temp_val = arr[i];
 			arr_pos = i;
 		}
 		i++;
 	}
-	free(totals);
-	return (arr[arr_pos]);
+	return (arr_pos);
 }
 
 t_2dpoint	**generate_points_arr(t_prog *prog)
