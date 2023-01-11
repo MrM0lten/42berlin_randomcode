718e9f30187e806b68560fd327edf43556ded39c
MrM0lten <jisserst@c4c1c1.42berlin.de>
2023-01-11 13:51:43 +0100

    fix norminette issues

--- Projects/minitalk/client.c
+++ Projects/minitalk/client.c
@@ -1,45 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisserst <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 13:37:42 by jisserst          #+#    #+#             */
+/*   Updated: 2023/01/11 13:37:43 by jisserst         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include "./libft/includes/libft.h"
 
-void send_bits(int pid, char c)
+void	send_bits(int pid, char c)
 {
-	int i;
-	int byte;
+	int	i;
+	int	byte;
 
 	i = 7;
-	while(i >= 0)
+	while (i >= 0)
 	{
 		byte = (c >> i) & 1L;
-		if(byte == 1)
-			kill(pid,SIGUSR1);
+		if (byte == 1)
+			kill(pid, SIGUSR1);
 		else
-			kill(pid,SIGUSR2);
+			kill(pid, SIGUSR2);
 		printf("%d", byte);
 		i--;
-		usleep(1000);
+		usleep(100);
 	}
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int pid;
-	int i;
+	int	pid;
+	int	i;
 
-	printf("im the client boiii\n");
-	if(ac != 3)
+	if (ac != 3)
 		return (0);
 	pid = ft_atoi(av[1]);
-
 	i = 0;
-	while(av[2][i])
+	while (av[2][i])
 	{
 		send_bits(pid, av[2][i]);
-		//usleep(100);
 		i++;
 	}
 	usleep(100);
-	send_bits(pid, '\n');
+	//send_bits(pid, '\n');
 	return (0);
 }
--- Projects/minitalk/server.c
+++ Projects/minitalk/server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisserst <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 13:37:29 by jisserst          #+#    #+#             */
+/*   Updated: 2023/01/11 13:37:30 by jisserst         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -5,41 +17,31 @@
 #include "./libft/includes/ft_printf.h"
 #include "./libft/includes/libft.h"
 
-int	flag = 1;
-
-void handler(int signal)
+void	handler(int signal)
 {
-	static int i;
-	static int c;
+	static int	i = 8;
+	static int	c = 0;
 
-	if (flag)
-	{
-		i = 8;
-		c = 0;
-		flag = 0;
-	}
 	if (signal == SIGUSR1)
 		c |= 1 << (i - 1);
 	i--;
-	if(i == 0)
+	if (i == 0)
 	{
-		ft_putchar_fd(c,STDOUT_FILENO);
+		ft_putchar_fd(c, STDOUT_FILENO);
 		i = 8;
 		c = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
- 	struct sigaction sa;
+	struct sigaction		sa;
 
 	sa.sa_handler = handler;
-	sigaction(SIGUSR1,&sa,NULL);
-	sigaction(SIGUSR2,&sa,NULL); 
-
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("PID = %d\n", getpid());
-	while(1)
+	while (1)
 		sleep(1);
-
 	return (0);
 }
