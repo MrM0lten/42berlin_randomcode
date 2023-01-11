4f2e16cee13cc200126d033c7bd43cce484e54a6
MrM0lten <jisserst@c4c1c1.42berlin.de>
2023-01-11 13:32:44 +0100

    almost done with minitalk

--- Projects/minitalk/client.c
+++ Projects/minitalk/client.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include "./libft/includes/libft.h"
 
-int main(void)
+void send_bits(int pid, char c)
 {
+	int i;
+	int byte;
+
+	i = 7;
+	while(i >= 0)
+	{
+		byte = (c >> i) & 1L;
+		if(byte == 1)
+			kill(pid,SIGUSR1);
+		else
+			kill(pid,SIGUSR2);
+		printf("%d", byte);
+		i--;
+		usleep(1000);
+	}
+}
+
+int main(int ac, char **av)
+{
+	int pid;
+	int i;
+
 	printf("im the client boiii\n");
+	if(ac != 3)
+		return (0);
+	pid = ft_atoi(av[1]);
+
+	i = 0;
+	while(av[2][i])
+	{
+		send_bits(pid, av[2][i]);
+		//usleep(100);
+		i++;
+	}
+	usleep(100);
+	send_bits(pid, '\n');
 	return (0);
 }
--- Projects/minitalk/server.c
+++ Projects/minitalk/server.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "./libft/includes/ft_printf.h"
+#include "./libft/includes/libft.h"
+
+int	flag = 1;
+
+void handler(int signal)
+{
+	static int i;
+	static int c;
+
+	if (flag)
+	{
+		i = 8;
+		c = 0;
+		flag = 0;
+	}
+	if (signal == SIGUSR1)
+		c |= 1 << (i - 1);
+	i--;
+	if(i == 0)
+	{
+		ft_putchar_fd(c,STDOUT_FILENO);
+		i = 8;
+		c = 0;
+	}
+}
 
 int main(void)
 {
-	printf("im the server\n");
+ 	struct sigaction sa;
+
+	sa.sa_handler = handler;
+	sigaction(SIGUSR1,&sa,NULL);
+	sigaction(SIGUSR2,&sa,NULL); 
+
+	ft_printf("PID = %d\n", getpid());
+	while(1)
+		sleep(1);
+
 	return (0);
 }
